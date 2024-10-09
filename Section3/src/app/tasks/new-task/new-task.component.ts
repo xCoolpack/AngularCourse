c74211155a69eee627a8648654503bf126defe70
xCoolpack <kolpapiotr@gmail.com>
2024-10-09 14:32:07 +0200

    Section 3

---
+++ Section3/src/app/tasks/new-task/new-task.component.ts
@@ -0,0 +1,34 @@
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { Task } from '../task/task.model';
+import { TasksService } from '../tasks.service';
+
+@Component({
+  selector: 'app-new-task',
+  templateUrl: './new-task.component.html',
+  styleUrl: './new-task.component.css',
+})
+export class NewTaskComponent {
+  @Input({ required: true }) userId!: string;
+  @Output() close = new EventEmitter<void>();
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
+  task!: Task;
+  private tasksService = inject(TasksService);
+
+  onCancel() {
+    this.close.emit();
+  }
+
+  onSubmit() {
+    this.tasksService.addTask(
+      {
+        title: this.enteredTitle,
+        summary: this.enteredSummary,
+        date: this.enteredDate,
+      },
+      this.userId
+    );
+    this.close.emit();
+  }
+}
---
+++ Section3/src/app/tasks/tasks.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input } from '@angular/core';
+import { TasksService } from './tasks.service';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css',
+})
+export class TasksComponent {
+  @Input({ required: true }) userId!: string;
+  @Input({ required: true }) name!: string;
+  isAddingTask = false;
+
+  constructor(private taskService: TasksService) {}
+
+  get selectedUserTasks() {
+    return this.taskService.getUserTasks(this.userId);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCloseAddTask() {
+    this.isAddingTask = false;
+  }
+}
