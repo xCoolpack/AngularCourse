b2c0c0d0789f52d35d657b749edf056183c488cf
Piotr Kołpa <kolpapiotr@gmail.com>
2024-11-24 19:50:09 +0100

    Section 13 p1

--- Section13/starting-project/src/app/auth/login/login.component.ts
+++ Section13/starting-project/src/app/auth/login/login.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  onSubmit(formData: NgForm) {
+    if (!formData.form.valid) {
+      return;
+    }
+
+    const enteredEmail = formData.form.value.email;
+    const enteredPassword = formData.form.value.password;
+  }
+}
